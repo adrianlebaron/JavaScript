90ec6315117b3246bf3e2883ed5d4a1362e9cc4c
A.D <adrianlebaron@gmail.com>
2018-11-12 09:35:35 -0700

    const variable

---
+++ constVariable.js
@@ -0,0 +1,15 @@
+let city = ‘Scottsdale’;
+console.log(city);
+
+city = ‘Scottsdale’;
+console.log(city);
+
+const city = ‘Scottsdale’;
+console.log(city);
+
+city = ‘Scottsdale’;
+console.log(city);
+
+const city = 'Scottsdale';
+
+console.log(city);
