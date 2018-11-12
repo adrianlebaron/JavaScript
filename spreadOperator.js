575e75698838f86f1f3cf11ccbe8a5ddb37b6873
A.D <adrianlebaron@gmail.com>
2018-11-12 14:33:22 -0700

    worked with bind and equal going to start new section

--- spreadOperator.js
+++ spreadOperator.js
@@ -11,19 +11,22 @@ console.log(shoppingCart); // [345, 375, 765, 123, 98, 123]
 
 // Copying Arrays
 const shoppingCart = [345, 375, 765, 123];
-const updatedCart = [...shoppingCart];
+const updatedCart = [...shoppingCart]; //shoppingCart.slice()
 updatedCart.push(5);
 console.log(updatedCart);
 console.log(shoppingCart);
 
+
+// using Math.max();
+
 const orderTotals = [1, 5, 1, 10, 2, 3];
-console.log(Math.max(...orderTotals));
+console.log(Math.max(...orderTotals));//if you do not have the spread operator it willl think it is just one value
 
-// [1, 5, 1, 10, 2, 3]
-// ...[1, 5, 1, 10, 2, 3]
-// 1, 5, 1, 10, 2, 3
+// [1, 5, 1, 10, 2, 3]      this
+// ...[1, 5, 1, 10, 2, 3]   pass in this
+// 1, 5, 1, 10, 2, 3        turns into this  
 
-const { starter, closer, ...relievers } = {
+const { starter, closer, ...relievers } = { //does not need to be called releivers
   starter: 'Verlander',
   closer: 'Giles',
   relief_1: 'Morton',
@@ -33,3 +36,14 @@ const { starter, closer, ...relievers } = {
 console.log(starter);
 console.log(closer);
 console.log(relievers);
+
+
+//exercise
+
+function yourTest() {
+  const highscore = [237.0198, 256.1, 234.846, 237.21, 256.654];
+  
+  return (Math.max(...highscore));
+}
+
+yourTest();
