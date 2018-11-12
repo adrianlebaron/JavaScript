233cb0e2291c9e3356ac29920b0dc17c709add2e
A.D <adrianlebaron@gmail.com>
2018-11-12 08:44:29 -0700

    implemented for loops foreach loops while loops and do while loops

--- loops.js
+++ loops.js
@@ -46,3 +46,68 @@ var players = [
 players.forEach(function(element) {
   console.log(element);
 });
+
+// How to Loop Through a JavaScript Object video loop thru an object
+
+
+var student = {
+  name: 'Kristine',
+  age: 12,
+  city: 'Scottsdale'
+};
+
+for (var key in student) {
+  console.log(key + " => " + student[key]);
+}
+
+// Guide to While and Do/While Loops in JavaScript video
+
+var players = [
+  'Altuve',
+  'Bregman',
+  'Correa',
+  'Springer'
+];
+
+var i = 0;
+while (i < players.length) {
+  console.log(players[i]);
+  i++;
+}
+
+// do while
+
+var players = [
+  'Altuve',
+  'Bregman',
+  'Correa',
+  'Springer'
+];
+
+var i = 21;
+do {
+  console.log(players[i]);
+  i++;
+} while (i < players.length)
+
+
+// both
+
+var players = [
+  'Altuve',
+  'Bregman',
+  'Correa',
+  'Springer'
+];
+
+var i = 0;
+while (i < players.length) {
+  console.log(players[i]);
+  i++;
+}
+
+var i = 21;
+do {
+  console.log(players[i]);
+  i++;
+} while (i < players.length)
