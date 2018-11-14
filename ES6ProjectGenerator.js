f6ba48caaf8c10f0e93d8f5178d678dc0df7cf15
A.D <adrianlebaron@gmail.com>
2018-11-14 14:37:24 -0700

    making the reduce function in java

---
+++ ES6ProjectGenerator.js
@@ -0,0 +1,4 @@
+// How to Utilize the ES6 Project Generator video
+
+// https://bottega.devcamp.com/full-stack-development-javascript-python/guide/1465 
+
---
+++ reduce.js
@@ -0,0 +1,23 @@
+// toThePowerOf(2, 3) => 8
+// reduce 
+
+// my try did not work
+// const toThePowerOf = (number, raisedTo) => {
+
+// for (let i = 0; i < raisedTo; i++) {
+//  [number * (raisedTo.length)]
+// }
+// toThePowerOf(2, 3)
+
+
+// Jordans example
+
+const toThePowerOf = (number, raisedTo) => {
+  const items = Array(exp).fill(num);
+  const reducer = (total, currentValue)
+  const reducer = (total, currentValue) => total * currentValue;
+  return items.reduce(reducer);
+}
+
+toThePowerOf(2, 3);//? 
+
