0d380a90a5701ded31da612c2d8a9d1f71960109
A.D <adrianlebaron@gmail.com>
2018-11-20 11:48:56 -0700

    made a function that pops and shifts until there is nothing left

--- weekSix.js
+++ weekSix.js
@@ -1,19 +1,32 @@
-let arr = ['girls', 'kitchen', 'construction', 'boys'];
+// const removeFirstAndLast = arr => {
+//   if  (arr.length >= 3) {
+//     return arr.slice(1, -1);
+//   }else {
+//     throw new Error("you need at least three element in the array");
+//   }
+// }
 
-var last = arr.pop();
-var first = arr.shift();
+// removeFirstAndLast(['girls', 'kitchen', 'construction', 'boys'])
+// removeFirstAndLast([1, 2, 3, 4,5])
 
-console.log(arr)
+// https://rails.devcamp.com/full-stack-development-javascript-python/guide/1652
+class ArrayPopper {
+  constructor(arr) {
+    this.arr = arr;
+    this.atBeginning = true;
+  }
 
-// Jordans
-
-const removeFirstAndLast = arr => {
-  if  (arr.length >= 3) {
-    return arr.slice(1, -1);
-  }else {
-    throw new Error("you need at least three element in the array");
+  togglePopper() {
+    this.atBeginning = !this.atBeginning;
+    return this.atBeginning ? this.arr.pop() : this.arr.shift();
   }
 }
 
-removeFirstAndLast(['girls', 'kitchen', 'construction', 'boys'])
-removeFirstAndLast([1, 2, 3, 4,5])
+const ap = new ArrayPopper([1, 2, 3, 4, 5]);
+
+ap.togglePopper(); //?
+ap.togglePopper(); //?
+ap.togglePopper(); //?
+ap.togglePopper(); //?
+ap.togglePopper(); //?
+ap.togglePopper(); //?
